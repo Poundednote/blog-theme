8546aca8100cd84a6bc7cd5f2337db2d2f3f0443
Poundednote <rayyan.naz.02@gmail.com>
2023-09-13 15:06:04 +0100

    * Created navbar
    Created navbar and added in the bootstrap classes inside functions.php.
    imported some scss styles from website-template

--- functions.php
+++ functions.php
@@ -1,6 +1,26 @@
 <?php
 
 add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/dist/main.js', [], '1.0.0', true);
     wp_enqueue_style('main', get_stylesheet_directory_uri() . '/assets/dist/main.css', [], '1.0.0', 'all');
+    wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/dist/main.js', [], '1.0.0', true);
 });
+
+
+register_nav_menu("blog-theme-menu", "the primary menu used throught blog-theme");
+
+// Add bootstraps nav class to wordpress navbar items
+function bootstrap_nav_item($classes, $item, $args, $depth)
+{
+    $classes[] = "nav-item";
+    return $classes;
+}
+
+add_filter('nav_menu_css_class', 'bootstrap_nav_item', 10, 4);
+
+function bootstrap_nav_link($atts, $item, $args, $depth)
+{
+    $atts['class'] = "nav-link";
+    return $atts;
+}
+
+add_filter('nav_menu_link_attributes', 'bootstrap_nav_link', 10, 4);
--- header.php
+++ header.php
@@ -3,58 +3,37 @@
 
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'blog_theme' ); ?></a>
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'blog_theme'); ?></a>
 
-    <header id="masthead" class="site-header">
-        <div class="site-branding">
-            <?php
-            the_custom_logo();
-            if ( is_front_page() && is_home() ) :
-                ?>
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                <?php
-            else :
-                ?>
-                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-                <?php
-            endif;
-            $blog_theme_description = get_bloginfo( 'description', 'display' );
-            if ( $blog_theme_description || is_customize_preview() ) :
-                ?>
-                <p class="site-description"><?php echo $blog-theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-            <?php endif; ?>
-        </div><!-- .site-branding -->
-
-        <nav class="navbar navbar-collapse navbar-dark bg-dark">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'blog-theme' ); ?></button>
-            <?php
-            wp_nav_menu(
-                array(
-                    'theme_location' => 'menu-1',
-                    'menu_id'        => 'primary-menu',
-                    'menu_class'        => 'navbar-nav',
-                    'walker' => new custom_boostrap_class_walker,
-                )
-            );
-
-            class custom_boostrap_class_walker extends Walker_Nav_Menu {
-                public function start_el(&$item_output, $item, $depth = 0, $args = null, $id = 0) {
-                    $item_output = preg_replace('/<li /', '<li class="nav-item"', $item_output, 1);
-                    $item_output = preg_replace('/<a /', '<a class="nav-link"', $item_output, 1);
-                    return $item_output;
-                }
-            }
-            ?>
-        </nav><!-- #site-navigation -->
-    </header><!-- #masthead -->
-
-<doctype
+        <header id="masthead" class="site-header">
+            <nav class="container-fluid navbar navbar-light navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand ms-5" href="<?php echo esc_url(home_url('/'));?>"><?php bloginfo('name'); ?></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <?php
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'blog-theme-menu',
+                                'menu_id'        => 'primary-menu',
+                                'menu_class'        => 'navbar-nav ms-auto me-5',
+                                'container' => null,
+                            )
+                        );
+                        ?>
+                    </div>
+                </div>
+            </nav><!-- #site-navigation -->
+        </header><!-- #masthead -->
