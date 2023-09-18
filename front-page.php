c01545ffaa88715439700d87bb11a205ea10e90f
Poundednote <rayyan.naz.02@gmail.com>
2023-09-18 09:06:55 +0100

    * Finished front page template
    completly finshed the styled layout of front-page.php

--- front-page.php
+++ front-page.php
@@ -3,43 +3,41 @@ get_header();
 ?>
 
 <main>
-    <div class="container bg-light" >
+    <div class="container-fluid bg-light" >
         <div class="row justify-content-center align-items-center" id="frontHero">
-        <div class="text-center hero-content col-5 lc-block">
-            <div class="lc-block">
-            <div editable="rich">
-                <h1 class="display-1"><?php bloginfo('name')?></h1>
+            <div class="text-center hero-content col-5 lc-block">
+                <div editable="rich">
+                    <h1 class="text-primary display-1"><?php bloginfo('name')?></h1>
+                </div>
+                <div editable="rich">
+                    <p class="lead"><?php bloginfo('description') ?></p>
+                </div>
             </div>
-            </div>
-            <div class="lc-block">
-            <div editable="rich">
-                <p class="lead"><?php bloginfo('description') ?></p>
-            </div>
-        </div>
         </div>
     </div>
 
 
-    <div class="container bg-light mt-5">
-        <div class="row text-center justify-content-center text-center align-items-start gap-5 p-5">
-            <h2 class="display-4 mb-0">Topics</h2>
+    <div class="container-fluid bg-tertiary py-5">
+        <div class="container">
+        <div class="row text-center justify-content-between text-center align-items-start gap-5 p-5">
+            <p class="display-4 mb-5">Topics</p>
             <?php
             foreach (array_slice(get_categories(), 0, 3) as $category) :
             ?>
-            <div class="col mx-5 col">
+            <div class="col my-2 mx-5">
                 <div>
-                    <h2 class="mb-3"><?php echo $category->name ?></h2>
+                    <h2 class="display-6 mb-3"><?php echo $category->name ?></h2>
                     <p><?php echo $category->description ?></p>
                 </div>
                 <div class="lc-block pt-4">
-                    <a class="btn btn-outline-primary" href="business.html" role="button">More</a>
+                    <a class="btn btn-primary" href="business.html" role="button">More</a>
                 </div>
             </div>
             <?php
             endforeach;
             ?>
-
         </div>
+    </div>
     </div>
 
     <?php
@@ -47,33 +45,65 @@ get_header();
         while(have_posts()) : the_post();
     ?>
     <div class="container bg-light p-5">
-        <div class="container">
-            <div class="row justify-content-between flex-column-reverse flex-md-row">
-                <div class="col-md-6 p-5">
-                <p class="text-primary display-4">Latest Post</p>
-                <h2 class="display-6"><?php the_title(); ?></h2>
-                <p<?php the_excerpt(); ?></p>
+        <div class="row justify-content-between gap-5 flex-column-reverse flex-lg-row">
+            <div class="d-flex flex-column col-lg-6 p-5">
+                <p class="display-3 mb-5">Latest Post</p>
+                <div class="stack">
+                    <div class="bg-primary underneath color-block">
+                    </div>
+                    <a id="latest-post" class="p-5 shadow my-auto" href=<?php the_permalink() ?>">
+                        <h2 class="display-6"><?php the_title(); ?></h2>
+                        <p<?php the_excerpt(); ?></p>
+                    </a>
+                </div>
             </div>
-            <div class="col-md-4">
+            <div class="mx-auto col-md-4 pt-5">
                 <?php has_post_thumbnail() ? the_post_thumbnail(array(500, 500)) : default_thumbnail(500, 500) ?>
             </div>
         </div>
     </div>
     <?php endwhile; ?>
 
-    <div class="container bg-light p-5">
-        <div class="container">
-            <div class="row gap-5 align-items-center justify-content-center flex-column-reverse flex-md-row align-items-center">
-                <p class="text-primary display-4">Popular Posts</p>
-                <?php
-                    query_posts( 'order=ASC&orderby=comment_count');
-                    while(have_posts()) : the_post();
-                ?>
-                <div class="col shadow">
-                    <h2 class="text-center"><?php the_title(); ?></h2>
-                    <?php has_post_thumbnail() ? the_post_thumbnail(array(200, 200)) : default_thumbnail(200, 200);?>
-                </div>
-                <?php endwhile; ?>
+    <div class="container bg-light py-5">
+        <p class="text-center display-3 mb-5">Whats Popular</p>
+            <div class="posts-grid">
+            <?php
+                query_posts( 'order=ASC&orderby=comment_count&posts_per_page=5');
+                if ( have_posts()) : while(have_posts()) : the_post();
+            ?>
+                <a class="post-card p-5 shadow my-auto" href=<?php the_permalink() ?>">
+                    <h2 class="display-6"><?php the_title(); ?></h2>
+                    <p<?php the_excerpt(); ?></p>
+                </a>
+        <?php
+            endwhile;
+            endif;
+        ?>
         </div>
     </div>
+
+    <div class="container-fluid cta bg-primary px-5">
+        <div class="row flex-column text-center py-5 justify-content-between align-items-center mb-3">
+            <h2 class="text display-3 mb-5">Dont Miss Out</h2>
+            <div class="col-6">
+                <p class="fw-bold">Sign Up here to our weekly newsletter to get a free item</p>
+            </div>
+            <div class="col col-md-4">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <div class="d-flex justify-content-center align-items-center">
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="john@doe.com">
+                    <button type="submit" class="w-25 btn btn-secondary">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container bg-light px-5">
+        <div class="row text-center py-5 justify-content-between align-items-center">
+            <h2 class="text display-3 mb-5">About</h2>
+        </div>
+    </div>
+
 </main>
+
+<?php get_footer() ?>
--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
 
@@ -16,13 +17,13 @@
         <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'blog_theme'); ?></a>
 
         <header id="masthead" class="site-header">
-            <nav class="container-fluid navbar navbar-light navbar-expand-lg bg-light">
-                <div class="container-fluid">
+            <nav class="container-fluid navbar py-4 navbar-expand-lg">
+                <div class="container-fluid mx-5">
                     <a class="navbar-brand ms-5" href="<?php echo esc_url(home_url('/'));?>"><?php bloginfo('name'); ?></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="collapse me-5 navbar-collapse" id="navbarNav">
                         <?php
                         wp_nav_menu(
                             array(
